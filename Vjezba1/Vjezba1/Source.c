04e8117fce0f3abc517d5663cd3b45789ed2ae7e
IvanMosa <imosic00@fesb.hr>
2023-10-11 18:30:53 +0200

    Added hello world

--- Vjezba1/Vjezba1/Source.c
+++ Vjezba1/Vjezba1/Source.c
@@ -1,8 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE (50)
+#define MAX_LINE (1024)
+
+typedef struct _student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+} Student;
+
 
 int main()
 {
-	printf("hello");
+	int noRows = 0;
+	FILE* filePtr = NULL;
+	char buffer[MAX_LINE] = { 0 };
 
+	filePtr = fopen("students.txt", "r");
+	if (!filePtr)
+	{
+		printf("Error while opening the file!\n");
+		return -1;
+	}
+	while (!feof(filePtr))
+	{
+		fgets(buffer, MAX_LINE, filePtr);
+		noRows++;
+	}
+
+
+	printf("%d", noRows);
+	fclose(filePtr);
 	return 0;
 }
